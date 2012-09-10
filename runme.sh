d7c9a9f9d9163d218ffb6599018e7aca0ade561c
Poul Flindt <pfl@eal.dk>
2012-09-10 19:47:14 +0200

    check connection to server using ping

--- runme.sh
+++ runme.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+if [ "$#" != 1 ]
+then
+	echo "Useage: 'runme.sh SERVERIP'"
+	exit 1
+fi
+
+if ping -w 2 -c 1 "$1" > /dev/null
+then 
+  echo Can ping "$1"
+else
+  echo Cannot ping "$1" - exiting
+  exit 1
+fi
+
 
