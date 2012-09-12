7a5d95321e381d492637e1a951140762c950f536
flindt <pfl@eal.dk>
2012-09-12 16:48:17 +0200

    copy specific network config + dhcp setup

--- runme.sh
+++ runme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+KEYFILE="sshkey/pxeboot"
+
 if [ "$#" != 1 ]
 then
 	echo "Useage: 'runme.sh SERVERIP'"
@@ -22,22 +24,31 @@ then
 	mkdir sshkey
 fi
 
-if [ ! -f ~/.ssh/pxeboot ]
+if [ ! -f $KEYFILE ]
 then
-	ssh-keygen -t rsa -N "" -q -f ~/.ssh/pxeboot > sshkey/log1.txt
+	ssh-keygen -t rsa -N "" -q -f $KEYFILE > sshkey/log1.txt
 	echo "Created new key pair"
 fi
-ssh-copy-id -i ~/.ssh/pxeboot.pub root@$1 > sshkey/log2.txt
+ssh-copy-id -i $KEYFILE.pub root@$1 > sshkey/log2.txt
 echo Copied public key to server
 
 # Install nescessary packages
 echo "Install packages..."
-ssh -i ~/.ssh/pxeboot root@$1 'apt-get install tftpd-hpa syslinux dhcp3-server'
+ssh -i $KEYFILE root@$1 'apt-get install tftpd-hpa syslinux dhcp3-server'
 
 
 # Setup network interface for the internal boot network - must be eth1
 echo "backup interfaces file"
-ssh -i ~/.ssh/pxeboot root@$1 'cp /etc/network/interfaces /etc/network/interfaces.old'
+ssh -i $KEYFILE root@$1 'cp /etc/network/interfaces /etc/network/interfaces.old'
+scp -i $KEYFILE interfaces root@$1:/etc/network/interfaces
+
+echo "backup dhcpd.conf file"
+ssh -i $KEYFILE root@$1 'cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.old'
+scp -i $KEYFILE dhcpd.conf root@192.168.122.64:/etc/dhcp/dhcpd.conf
+
+
+
+
 
 
 
